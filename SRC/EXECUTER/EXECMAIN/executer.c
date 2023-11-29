ed3d69bf40eab80b6b2691b77eacc3a22c4fbd3c
senemyilmazz <senemy97@gmail.com>
2023-11-29 14:09:21 +0300

    komut pathleri eklendi

--- SRC/EXECUTER/EXECMAIN/executer.c
+++ SRC/EXECUTER/EXECMAIN/executer.c
@@ -6,7 +6,7 @@
 /*   By: senyilma <senyilma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 00:05:05 by senyilma          #+#    #+#             */
-/*   Updated: 2023/11/29 04:15:03 by senyilma         ###   ########.fr       */
+/*   Updated: 2023/11/29 13:47:31 by senyilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	executer(t_prime *g_prime)
 {
-	int			*fd[2];
+	int			fd[2];
 	t_parser	*parser;
 
 	if (!g_prime)
@@ -25,16 +25,15 @@ void	executer(t_prime *g_prime)
 		if (g_prime->cmd_count > 1)
 			pipe(fd);
 		run_command(g_prime, parser, fd);
+		//if (fd[0] && fd[1])
+		//{
+		//	close(fd[0]);
+		//	close(fd[1]);
+		//	fd[0] = 0;
+		//	fd[1] = 0;
+		//}
 		parser = parser->next;
-		if (fd[0] && fd[1])
-		{
-			close(fd[0]);
-			close(fd[1]);
-			fd[0] = 0;
-			fd[1] = 0;
-		}
 	}
-	free(fd);
 }
 
 void	run_command(t_prime *g_prime, t_parser *parser, int *fd)
@@ -99,42 +98,57 @@ void	run_builtin(t_prime *g_prime, int built_type, int *fd)
 void	run_execve(t_prime *g_prime, t_parser *parser, int *fd)
 {
 	char	**env_list;
+	char	*command;
 
+	command = NULL;
 	parser->pid = fork();
 	if (parser->pid == 0)
 	{
-		dup_stdio(g_prime, parser, fd);
 		env_list = get_env_cpy(g_prime);
-		execve(parser->command, parser->parameters, env_list);
+		command = get_command(g_prime, parser);
+		dup_stdio(g_prime, parser, fd);
+		execve(command, parser->parameters, env_list);
 		if (parser->command)
 			print_error(parser->command, " command not found!\n");
 		free_env_cpy(env_list);
 		free_prime(g_prime);
+		free(command);
 		exit(127);
 	}
+	free(command);
 	waitpid(parser->pid, &g_prime->exit_code, 0);
-	g_prime->exit_code = WEXITSTATUS(g_core.exit_code);
+	g_prime->exit_code = WEXITSTATUS(g_prime->exit_code);
 }
 
 void	dup_stdio(t_prime *g_prime, t_parser *parser, int *fd)
 {
-	/*int	new_fd[2];
+	int	new_fd[2];
 
-	if (g_prime->parser->infile == -2)
+	if (parser->infile == -2)
 	{
 		pipe(new_fd);
 		write(new_fd[1], g_prime->parser->heredoc, \
 			ft_strlen(g_prime->parser->heredoc));
-		dup2(new_fd[1], STDOUT_FILENO);
 		close (new_fd[1]);
+		dup2(new_fd[0], STDOUT_FILENO);
 		close(new_fd[0]);
-	}*/
+	}
 	if (parser->infile > 2)
-		dup2(g_prime->parser->infile, STDIN_FILENO);
-	else if (g_prime->cmd_count > 1  && parser != g_prime->parser)
+		dup2(parser->infile, STDIN_FILENO);
+	else if (g_prime->cmd_count > 1 && parser != g_prime->parser)
+	{
+		close (new_fd[1]);
 		dup2(fd[0], STDIN_FILENO);
+		close (new_fd[0]);
+	}
 	if (parser->outfile > 2)
-		dup2(g_prime->parser->outfile, STDOUT_FILENO);
+	{
+		dup2(parser->outfile, STDOUT_FILENO);
+	}
 	else if (g_prime->cmd_count > 1 && parser->next)
+	{
+		close (new_fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
+		close (new_fd[1]);
+	}
 }
