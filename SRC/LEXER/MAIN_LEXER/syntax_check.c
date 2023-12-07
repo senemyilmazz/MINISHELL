ed679cf50d44c681fc8786ac8489ad5548693ce1
senemyilmazz <senemy97@gmail.com>
2023-12-07 18:41:13 +0300

    .

--- SRC/LEXER/MAIN_LEXER/syntax_check.c
+++ SRC/LEXER/MAIN_LEXER/syntax_check.c
@@ -6,7 +6,7 @@
 /*   By: senyilma <senyilma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 22:35:16 by senyilma          #+#    #+#             */
-/*   Updated: 2023/12/03 22:30:38 by senyilma         ###   ########.fr       */
+/*   Updated: 2023/12/07 17:32:35 by senyilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 int	syn_error(t_prime *g_prime, int temp_type, int temp_len)
 {
 	if (temp_type == -1)
-		return (synerr_print(g_prime, "||"));
+		return (synerr_print(g_prime, "||", 0));
 	else if (temp_type == -2 && temp_len == 3)
-		return (synerr_print(g_prime, ">"));
+		return (synerr_print(g_prime, ">", 0));
 	else if (temp_type == -2 && temp_len > 3)
-		return (synerr_print(g_prime, ">>"));
+		return (synerr_print(g_prime, ">>", 0));
 	else if (temp_type == -3 && temp_len == 3)
-		return (synerr_print(g_prime, "<"));
+		return (synerr_print(g_prime, "<", 0));
 	else if (temp_type == -3 && temp_len < 5)
-		return (synerr_print(g_prime, "<<"));
+		return (synerr_print(g_prime, "<<", 0));
 	else if (temp_type == -3 && temp_len > 5)
-		return (synerr_print(g_prime, "<<<"));
+		return (synerr_print(g_prime, "<<<", 0));
 	return (0);
 }
 
@@ -46,12 +46,12 @@ int	syntax_check(t_prime *g_prime)
 		else if (g_prime->lexer->type == SIGN_PIPE || \
 			(temp->next && temp->next->type == SIGN_PIPE && t > 0) || \
 			(temp->type == SIGN_PIPE && !temp->next))
-			return (synerr_print(g_prime, "|"));
+			return (synerr_print(g_prime, "|", 0));
 		else if ((t >= SIGN_SOR && t <= 5) && (!temp->next || temp->next->type))
 			flag = 2;
 		temp = temp->next;
 	}
 	if (flag == 2)
-		return (synerr_print(g_prime, "newline"));
+		return (synerr_print(g_prime, "newline", 0));
 	return (0);
 }
