9d656538a6ed41c850343adf0ee269e52cf97d28
senemyilmazz <senemy97@gmail.com>
2023-12-07 18:38:03 +0300

    .

--- SRC/UTILS/print_errors.c
+++ SRC/UTILS/print_errors.c
@@ -6,15 +6,18 @@
 /*   By: senyilma <senyilma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 21:39:21 by senyilma          #+#    #+#             */
-/*   Updated: 2023/12/07 10:26:57 by senyilma         ###   ########.fr       */
+/*   Updated: 2023/12/07 17:31:10 by senyilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../INCLUDE/minishell.h"
 
-int	synerr_print(t_prime *g_prime, char *str)
+int	synerr_print(t_prime *g_prime, char *str, int f)
 {
-	write(2, "minikkus: syntax error near unexpected token ", 51);
+	if (f == 1)
+		write(2, "minikkus: ", 11);
+	else
+		write(2, "minikkus: syntax error near unexpected token ", 51);
 	write(2, str, ft_strlen(str));
 	write(2, "\n", 1);
 	g_prime->exit_code = 258;
@@ -48,8 +51,11 @@ int	command_error(char *arg, char *cmd, char *str, t_prime *g_prime)
 		write(2, cmd, ft_strlen(cmd));
 	write(2, ": ", 2);
 	if (arg)
+	{
 		write(2, arg, ft_strlen(arg));
-	write(2, ": ", 2);
+		write(2, ": ", 2);
+
+	}
 	write(2, str, ft_strlen(str));
 	write(2, "\n", 1);
 	g_prime->exit_code = 1;
--- SRC/UTILS/print_tables.c
+++ SRC/UTILS/print_tables.c
@@ -1,3 +1,6 @@
+#include "../../INCLUDE/minishell.h"
+
+
 void	print_lexer(t_prime	*g_prime)
 {
 	t_lexer	*temp;
@@ -45,33 +48,20 @@ void	print_parser(t_prime *g_prime)
 	}
 }
 
-void	print_parser(t_prime *g_prime)
+void	print_expander(t_prime	*g_prime)
 {
-	t_parser	*temp;
-	int			i;
+	t_expander	*temp;
 
-	if (!g_prime->parser)
-		return ;
-	temp = g_prime->parser;
-	printf("\n-----------{ PARSER }----------\n");
+	temp = g_prime->expander;
+	printf("\n---------{ EXPANDER }----------\n");
 	while (temp)
 	{
-		printf("command: %s\n", temp->command);
-		printf("args: ");
-		i = -1;
-		while (temp->parameters && temp->parameters[++i])
-			printf(" %s ; ", temp->parameters[i]);
-		printf("\ninfile: %d\n", temp->infile);
-		printf("outfile: %d\n", temp->outfile);
-		while (temp->file)
-		{
-			printf("{filename: %s ; re_type %d}", \
-				temp->file->filename, temp->file->re_type);
-			temp->file = temp->file->next;
-		}
-		if (temp->heredoc)
-			printf("\nheredoc: %s", temp->heredoc);
-		printf("\n-------------------------------\n");
+		printf("C:%s\nT:%d\n", temp->content, temp->type);
+		printf("env:%d\n", temp->env);
+		if (temp->ex_content)
+			printf("ex_cont:%s\n", temp->ex_content);
+		printf("-------------------------------\n");
 		temp = temp->next;
 	}
+	printf("\n\n");
 }
